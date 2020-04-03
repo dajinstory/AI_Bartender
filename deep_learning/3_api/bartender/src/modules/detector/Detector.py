e0c2e074d645b5054390522d75beaec596fdf46d
Dajin Han <dajinstory@naver.com>
2020-04-03 23:45:40 +0900

    x,y coords....

--- deep_learning/3_api/bartender/src/modules/detector/Detector.py
+++ deep_learning/3_api/bartender/src/modules/detector/Detector.py
@@ -6,6 +6,7 @@ import cv2
 from keras.backend.tensorflow_backend import set_session
 import numpy as np
 import tensorflow as tf
+import cv2
 
 from SSD import *
 from SSD.ssd import SSD300
@@ -24,10 +25,11 @@ class Detector:
                'Sheep', 'Sofa', 'Train', 'Tvmonitor']
 
         # variables for preprocessing input image
-        self.input_shape=(300, 300, 3)
-        self.grids=[1,2,4,7]
+        self.resolution=(300, 300, 3)
+        self.grids=[1,2]
         self.margin_rate=0.3
         self.accuracy=0.5
+        self.iou_threshold=0.5
 
         # detector and utilities
         self.detector = self.load_detector()
@@ -44,28 +46,25 @@ class Detector:
 
         # load ssd
         NUM_CLASSES = len(self.classes) + 1
-        detector = SSD300(self.input_shape, num_classes=NUM_CLASSES)
+        detector = SSD300(self.resolution, num_classes=NUM_CLASSES)
         detector.load_weights('./SSD/model/weights_SSD300.hdf5')
 
         return detector
 
     # divide image by grid
-    def divide_by_grid(self, image, grids=None, margin_rate=None, img_resolution=None):
+    def divide_by_grid(self, image, grids=None, margin_rate=None):
         # parameter setting with default value
         if grids is None:
             grids = self.grids
         if margin_rate is None:
             margin_rate = self.margin_rate
-        if img_resolution is None:
-            img_resolution = self.input_shape
 
-        img_w, img_h = image.shape[1:3]
+        img_h, img_w = image.shape[0:2]
+        image = image.reshape(img_h, img_w, 3)
 
         # result
-        new_image = []
-        new_coord = []
-
-        image = image.reshape(img_w, img_h, 3)
+        new_images = []
+        new_coords = []
 
         # divide image by grid
         for grid in grids:
@@ -76,51 +75,59 @@ class Detector:
             stride_y = int(len_y * (1 - margin_rate))
 
             # slice image according to current grid
-            for idx_x in range(grid):
-                for idx_y in range(grid):
+            for idx_y in range(grid):
+                for idx_x in range(grid):
                     # get coord of current sliced image
                     p_x = idx_x * stride_x
                     p_y = idx_y * stride_y
-                    new_coord.append([p_x, p_y, p_x + len_x, p_y + len_y])
+                    new_coords.append({
+                        'x':p_x,
+                        'y':p_y,
+                        'len_x':len_x,
+                        'len_y':len_y
+                    })
 
                     # get current sliced image
-                    image_sliced = image[p_x:p_x + len_x, p_y:p_y + len_y:]
-                    image_sliced_resized = cv2.resize(image_sliced, img_resolution)
-                    new_image.append(image_sliced_resized)
+                    image_sliced = image[p_y:p_y + len_y, p_x:p_x + len_x:]
+                    image_sliced_resized = cv2.resize(image_sliced, self.resolution[:2])
+                    new_images.append(image_sliced_resized)
 
-        return np.array(new_image), np.array(new_coord)
+        return np.array(new_images), np.array(new_coords)
 
     # use nms to get the object with highest confidence in each area
-    def nms(self, bounding_boxes, confidence_score, labels, threshold):
+    def nms(self, boxes, scores, labels, iou_threshold=None):
+        if iou_threshold is None:
+            iou_threshold = self.iou_threshold
+
         # If no bounding boxes, return empty list
-        if len(bounding_boxes) == 0:
+        if len(boxes) == 0:
             return [], [], []
 
         # Bounding boxes
-        boxes = np.array(bounding_boxes)
+        boxes = np.array(boxes)
 
         # coordinates of bounding boxes
-        start_x = boxes[:, 0]
-        start_y = boxes[:, 1]
-        end_x = boxes[:, 2]
-        end_y = boxes[:, 3]
+        start_xs = boxes[:, 1]
+        start_ys = boxes[:, 0]
+        end_xs = boxes[:, 3]
+        end_ys = boxes[:, 2]
 
         # Confidence scores of bounding boxes
-        score = np.array(confidence_score)
+        scores = np.array(scores)
 
         # each box's labels
-        label = np.array(labels)
+        labels = np.array(labels)
 
         # Picked bounding boxes
         picked_boxes = []
-        picked_score = []
-        picked_label = []
+        picked_scores = []
+        picked_labels = []
 
         # Compute areas of bounding boxes
-        areas = (end_x - start_x + 1) * (end_y - start_y + 1)
+        areas = (end_xs - start_xs + 1) * (end_ys - start_ys + 1)
 
         # Sort by confidence score of bounding boxes
-        order = np.argsort(score)
+        order = np.argsort(scores)
 
         # Iterate bounding boxes
         while order.size > 0:
@@ -128,15 +135,15 @@ class Detector:
             index = order[-1]
 
             # Pick the bounding box with largest confidence score
-            picked_boxes.append(bounding_boxes[index])
-            picked_score.append(confidence_score[index])
-            picked_label.append(label[index])
+            picked_boxes.append(boxes[index])
+            picked_scores.append(scores[index])
+            picked_labels.append(labels[index])
 
             # Compute ordinates of intersection-over-union(IOU)
-            x1 = np.maximum(start_x[index], start_x[order[:-1]])
-            x2 = np.minimum(end_x[index], end_x[order[:-1]])
-            y1 = np.maximum(start_y[index], start_y[order[:-1]])
-            y2 = np.minimum(end_y[index], end_y[order[:-1]])
+            x1 = np.maximum(start_xs[index], start_xs[order[:-1]])
+            x2 = np.minimum(end_xs[index], end_xs[order[:-1]])
+            y1 = np.maximum(start_ys[index], start_ys[order[:-1]])
+            y2 = np.minimum(end_ys[index], end_ys[order[:-1]])
 
             # Compute areas of intersection-over-union
             w = np.maximum(0.0, x2 - x1 + 1)
@@ -146,22 +153,28 @@ class Detector:
             # Compute the ratio between intersection and union
             ratio = intersection / (areas[index] + areas[order[:-1]] - intersection)
 
-            left = np.where(ratio < threshold)
+            left = np.where(ratio < iou_threshold)
             order = order[left]
-        return picked_boxes, picked_score, picked_label
+        return picked_boxes, picked_scores, picked_labels
 
     # converge the objects from images(sliced by grids) to REAL_OBJECTS in input shape
-    def transform_detection_result(self, divided_images, coord):
-        boxs = []
+    def transform_detection_result(self, results, coords, accuracy=None):
+        if accuracy is None:
+            accuracy = self.accuracy
+
+        boxes = []
         scores = []
         labels = []
-        for i, img in enumerate(divided_images):
+
+        # for each grid cells
+        for i, img in enumerate(results):
             det_label = results[i][:, 0]
             det_conf = results[i][:, 1]
-            det_xmin = results[i][:, 2]
-            det_ymin = results[i][:, 3]
-            det_xmax = results[i][:, 4]
-            det_ymax = results[i][:, 5]
+            det_xmin = results[i][:, 3]
+            det_ymin = results[i][:, 2]
+            det_xmax = results[i][:, 5]
+            det_ymax = results[i][:, 4]
+
             # Get detections with confidence higher than accuracy
             top_indices = [i for i, conf in enumerate(det_conf) if conf >= accuracy]
             top_conf = det_conf[top_indices]
@@ -170,46 +183,54 @@ class Detector:
             top_ymin = det_ymin[top_indices]
             top_xmax = det_xmax[top_indices]
             top_ymax = det_ymax[top_indices]
-            x_rate = (coord[i][2] - coord[i][0]) / 300
-            y_rate = (coord[i][3] - coord[i][1]) / 300
+
+            # for each objects in current grid
             for j in range(top_conf.shape[0]):
-                xmin = int(round(top_xmin[j] * img.shape[1]) * x_rate) + coord[i][1]
-                ymin = int(round(top_ymin[j] * img.shape[0]) * y_rate) + coord[i][0]
-                xmax = int(round(top_xmax[j] * img.shape[1]) * x_rate) + coord[i][1]
-                ymax = int(round(top_ymax[j] * img.shape[0]) * y_rate) + coord[i][0]
+                xmin = int(round(top_xmin[j] * coords[i]['len_x']) + coords[i]['x'])
+                ymin = int(round(top_ymin[j] * coords[i]['len_y']) + coords[i]['y'])
+                xmax = int(round(top_xmax[j] * coords[i]['len_x']) + coords[i]['x'])
+                ymax = int(round(top_ymax[j] * coords[i]['len_y']) + coords[i]['y'])
+
                 score = top_conf[j]
                 label = int(top_label_indices[j])
-                label_name = classes[label - 1]
+                label_name = self.classes[label - 1]
 
-                boxs.append([xmin, ymin, xmax, ymax])
+                boxes.append([xmin, ymin, xmax, ymax])
                 scores.append(score)
                 labels.append(label_name)
-        box_nms, score_nms, label_nms = nms(boxs, scores, labels, iou_threshold)
 
-        # [{'x':x, 'y':y, 'len_x':len_x, 'len_y':len_y, 'label':label}]
-        result = []
-        for i in range(len(scores)):
-            result.append(pack('iiiis', box_nms[i][0], box_nms[i][1], box_nms[i][2] - box_nms[i][0],
-                               box_nms[i][3] - box_nms[i][1], label_nms[i]))
-        return result
+        boxes_nms, scores_nms, labels_nms = self.nms(boxes, scores, labels)
+
+        # get real coord of each objects
+        objects = []
+        for i in range(len(scores_nms)):
+            objects.append({
+                'x':boxes_nms[i][0],
+                'y':boxes_nms[i][1],
+                'len_x':boxes_nms[i][2] - boxes_nms[i][0],
+                'len_y':boxes_nms[i][3] - boxes_nms[i][1],
+                'label':labels_nms[i]
+            })
+        return objects
 
     # Main Function
     def get_objects(self, image):
 
         # divide images
-        divided_images, coord = self.divide_by_grid(image)
+        divided_images, coords = self.divide_by_grid(image)
 
         # get objects detected from all sliced images by grid
         batch_size = 1
         verbose = 1
         preds = self.detector.predict(divided_images, batch_size, verbose)
-        results = self.bbox_util.detection_out(preds)
+        results = self.bbox_util.detection_out(preds) # 0.0-1.0 position in each grid cell
 
-        # nms objects form each slice images, and then get the real coord in input image
-        return self.transform_detection_result(results, coord)
+        # nms objects form each slice images, and then get the real coords in input image
+        return self.transform_detection_result(results, coords)
 
 
 if __name__ == "__main__":
+    image = cv2.imread('../../../../../images/2.jpg', cv2.IMREAD_COLOR)
     detector = Detector()
-    result = detector.get_objects('../../../../../images/1.jpg')
+    result = detector.get_objects(image)
     print(result)
